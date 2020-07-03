8affd0065d01f8e238fb869b9ed0a6c33af63299
Pierce de Jong <pierce.dejong45@gmail.com>
2020-07-03 00:47:42 -0600

    possiblty see images now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
