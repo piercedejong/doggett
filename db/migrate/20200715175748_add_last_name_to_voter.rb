a5dc5ee16c588eea5eea6d8ddebe94fe215ece22
Pierce de Jong <pierce.dejong45@gmail.com>
2020-07-15 16:13:04 -0600

    fixed migration

--- db/migrate/20200715175748_add_last_name_to_voter.rb
+++ db/migrate/20200715175748_add_last_name_to_voter.rb
@@ -2,6 +2,5 @@ class AddLastNameToVoter < ActiveRecord::Migration[6.0]
   def change
     add_column :voters, :last_name, :string
     add_column :voters, :middle_name, :string, default: ""
-    change_column :voters, :name, :first_name
   end
 end
