4479cb6f3e212d13cc291815a3734b36be5b3224
Pierce de Jong <pierce.dejong45@gmail.com>
2020-07-16 16:46:52 -0600

    updates

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,9 +3,9 @@ class CallsController < ApplicationController
     def index
         @voter = current_user.voters.where(status: "Not Called Yet").first
         if @voter
-            
+
         else
-            redirect_to root_path, alert: "There are currently no voters to call"
+            redirect_to root_path, alert: "There are currently no voters to call. Please contact Pierce @ pierce.dejong45@gmail.com or (403) 619-2494"
         end
 
     end
